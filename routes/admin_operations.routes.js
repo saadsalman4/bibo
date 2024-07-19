99965836c92ee47aa3ee48b86e8073a79f2f1e41
saadsalman4 <139686182+saadsalman4@users.noreply.github.com>
2024-07-18 22:41:21 -0700

    10:41

--- routes/admin_operations.routes.js
+++ routes/admin_operations.routes.js
@@ -5,19 +5,19 @@ const adminAuth = require('../middlewares/adminAuthCheck')
 
 const router = express.Router();
 
-router.post('/addAdmin', addAdmin)
+router.post('/addAdmin', addAdmin) //manual add
 
 router.post("/login", login);
 router.post('/forgot-password', forgotPassword)
 
 router.get('/product-count', adminAuth, getProductCount)
-router.get('/order-count', getOrderCount)
-router.get('/product-details', getProductDetails)
-router.get('/order-details', getOrderDetails)
+router.get('/order-count', adminAuth, getOrderCount)
+router.get('/product-details', adminAuth, getProductDetails)
+router.get('/order-details', adminAuth, getOrderDetails)
 
-router.get('/get-users', getAllUsers)
-router.post('/block-user/:id', blockUser)
-router.post('/block-product/:id', blockProduct)
-router.get('/order-history/:id', orderHistory)
+router.get('/get-users',adminAuth, getAllUsers)
+router.post('/block-user/:id', adminAuth, blockUser)
+router.post('/block-product/:id', adminAuth, blockProduct)
+router.get('/order-history/:id', adminAuth, orderHistory)
 
 module.exports = router;
